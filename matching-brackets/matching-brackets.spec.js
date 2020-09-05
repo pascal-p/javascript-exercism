731fab04d6991ba924e4291c316122514ef5e2e0
Pascal P <lacsap_666@yahoo.fr>
2020-09-05 20:15:17 +1200

    change matcher for boolean

--- matching-brackets/matching-brackets.spec.js
+++ matching-brackets/matching-brackets.spec.js
@@ -2,67 +2,67 @@ import { isPaired } from './matching-brackets';
 
 describe('Matching Brackets', () => {
   test('paired square brackets', () => {
-    expect(isPaired('[]')).toEqual(true);
+    expect(isPaired('[]')).toBe(true);
   });
 
   test('empty string', () => {
-    expect(isPaired('')).toEqual(true);
+    expect(isPaired('')).toBe(true);
   });
 
   test('unpaired brackets', () => {
-    expect(isPaired('[[')).toEqual(false);
+    expect(isPaired('[[')).toBe(false);
   });
 
   test('wrong ordered brackets', () => {
-    expect(isPaired('}{')).toEqual(false);
+    expect(isPaired('}{')).toBe(false);
   });
 
   test('wrong closing bracket', () => {
-    expect(isPaired('{]')).toEqual(false);
+    expect(isPaired('{]')).toBe(false);
   });
 
   test('paired with whitespace', () => {
-    expect(isPaired('{ }')).toEqual(true);
+    expect(isPaired('{ }')).toBe(true);
   });
 
   test('partially paired brackets', () => {
-    expect(isPaired('{[])')).toEqual(false);
+    expect(isPaired('{[])')).toBe(false);
   });
 
   test('simple nested brackets', () => {
-    expect(isPaired('{[]}')).toEqual(true);
+    expect(isPaired('{[]}')).toBe(true);
   });
 
   test('several paired brackets', () => {
-    expect(isPaired('{}[]')).toEqual(true);
+    expect(isPaired('{}[]')).toBe(true);
   });
 
   test('paired and nested brackets', () => {
-    expect(isPaired('([{}({}[])])')).toEqual(true);
+    expect(isPaired('([{}({}[])])')).toBe(true);
   });
 
   test('unopened closing brackets', () => {
-    expect(isPaired('{[)][]}')).toEqual(false);
+    expect(isPaired('{[)][]}')).toBe(false);
   });
 
   test('unpaired and nested brackets', () => {
-    expect(isPaired('([{])')).toEqual(false);
+    expect(isPaired('([{])')).toBe(false);
   });
 
   test('paired and wrong nested brackets', () => {
-    expect(isPaired('[({]})')).toEqual(false);
+    expect(isPaired('[({]})')).toBe(false);
   });
 
   test('paired and incomplete brackets', () => {
-    expect(isPaired('{}[')).toEqual(false);
+    expect(isPaired('{}[')).toBe(false);
   });
 
   test('too many closing brackets', () => {
-    expect(isPaired('[]]')).toEqual(false);
+    expect(isPaired('[]]')).toBe(false);
   });
 
   test('math expression', () => {
-    expect(isPaired('(((185 + 223.85) * 15) - 543)/2')).toEqual(true);
+    expect(isPaired('(((185 + 223.85) * 15) - 543)/2')).toBe(true);
   });
 
   test('complex latex expression', () => {
@@ -70,20 +70,20 @@ describe('Matching Brackets', () => {
       isPaired(
         '\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)'
       )
-    ).toEqual(true);
+    ).toBe(true);
   });
 
   // more ...
   test('no brackets at all', () => {
-    expect(isPaired('sdfhgkasdhfkashkd, 9sjdh, kahsdk9shdkajhd')).toEqual(true);
+    expect(isPaired('sdfhgkasdhfkashkd, 9sjdh, kahsdk9shdkajhd')).toBe(true);
   });
 
   test('some well formed brackets', () => {
-    expect(isPaired('[(sdfhgkasdhfkashkd (9sjdh) kahsdk9shdkajhd)][{{}}]asasas')).toEqual(true);
+    expect(isPaired('[(sdfhgkasdhfkashkd (9sjdh) kahsdk9shdkajhd)][{{}}]asasas')).toBe(true);
   });
 
   test('some non well formed brackets', () => {
-    expect(isPaired('[(sdfhgkasdhfkashkd (9sjdh) kahsdk9shdkajhd)]{{}]asasas')).toEqual(false);
+    expect(isPaired('[(sdfhgkasdhfkashkd (9sjdh) kahsdk9shdkajhd)]{{}]asasas')).toBe(false);
   });
 
 });
