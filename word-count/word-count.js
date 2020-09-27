eb8517db2ba77080a1250010b8f3a9f98fc6a137
Pascal P <lacsap_666@yahoo.fr>
2020-09-28 10:37:21 +1300

    word-count review

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -6,7 +6,7 @@
 
 const PUNCTUATION = [':', ';', '\\\?', '!', ',', '\\\.']; // but single-quote
 
-export const countWords = (str) => {
+export const countWords_V1 = (str) => {
   let wc = {} // new Map();
 
   // first, replace any punctuation symbols
@@ -14,7 +14,7 @@ export const countWords = (str) => {
   const punctRE = new RegExp(expr, 'g')
   str = str.replace(punctRE, ' ');  // replaceAll() not yet supported (but firefox)
 
-  // second make sur the str is space separated (1 space only) and trimmed
+  // second make sure the str is space separated (1 space only) and trimmed
   const allSpacesRE = /(\t|\n|\s|_)+/g;
   str = str.replace(allSpacesRE, ' ').trim();
 
@@ -27,19 +27,43 @@ export const countWords = (str) => {
   for (let word of str.split(' ')) {
     if (word.match(wordRE)) {
       updateFn(word.toLowerCase());
-      continue;
     }
-
-    if (word.startsWith("'") && word.endsWith("'")) {
+    else if (word.startsWith("'") && word.endsWith("'")) {
       updateFn(word.replace(/^'/, '').replace(/'$/, '').toLowerCase());
-      continue;
     }
-
-    if (word.match(wordMixedRE)) {
+    else if (word.match(wordMixedRE)) {
       updateFn(word.toLowerCase());
-      continue;
     }
   };
 
   return wc;
+};
+
+export const countWords = (str) => {
+  // first, replace any punctuation symbols
+  const expr = PUNCTUATION.map((s) => s + '|').join('').replace(/\|$/, '');
+  const punctRE = new RegExp(expr, 'g')
+  str = str.replace(punctRE, ' ');
+
+  // second make sure the str is space separated (1 space only) and trimmed
+  const allSpacesRE = /(\t|\n|\s|_)+/g;
+  str = str.replace(allSpacesRE, ' ').trim();
+
+  const updateFn = (wc, w) => wc[w] = wc[w] === undefined ? 1 : wc[w] + 1;
+
+  return str.split(' ').reduce(
+    (wc, word) => {
+      if (word.match(/^\w+$/i)) {          // no single-quote
+        updateFn(wc, word.toLowerCase());
+      }
+      else if (word.startsWith("'") && word.endsWith("'")) {
+        updateFn(wc, word.replace(/^'/, '').replace(/'$/, '').toLowerCase());
+      }
+      else if (word.match(/^[\w']+$/i)) {  // potentially multi-single quotes
+        updateFn(wc, word.toLowerCase());
+      }
+      return wc;
+    },
+    {}
+  );
 };
