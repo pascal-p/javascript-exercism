f864abf2f9b5b55b5d0993acea4958ce79d7dda0
Pascal P <lacsap_666@yahoo.fr>
2020-09-28 11:38:39 +1300

    difference-of-squares FP version

--- difference-of-squares/difference-of-squares.js
+++ difference-of-squares/difference-of-squares.js
@@ -12,18 +12,18 @@ export class Squares {
 
   get sumOfSquares() {
     // 1² + 2² + 3² + ... + n² ≡ (n × (n + 1) × (2n + 1)) / 6
-    let x1 = this.n;
-    let x2 = x1 + 1;
-    let x3 = (this.n << 1) + 1;
-    let x = (x1 * x2 * x3) >> 1;
+    const x1 = this.n;
+    const x2 = this.n + 1; // x1 + 1;
+    const x3 = (this.n << 1) + 1;
+    const x = (x1 * x2 * x3) >> 1;
     return x / 3;
   }
 
   get squareOfSum() {
     // sum(1:n)^2 ≡ (n × (n + 1) / 2)²
-    let x1 = this.n;
-    let x2 = x1 + 1;
-    let x = (x1 * x2) >> 1;
+    const x1 = this.n;
+    const x2 = this.n + 1; // x1 + 1;
+    const x = (x1 * x2) >> 1;
     return x * x;
   }
 
@@ -31,3 +31,47 @@ export class Squares {
     return this.squareOfSum - this.sumOfSquares;
   }
 }
+
+//
+// functional version
+//
+const add1 = (x) => x + 1;
+
+const mult2Add1 = (x) => (x << 1) + 1;
+
+const multTriDiv2 = (x1, x2, x3) => (x1 * x2 * x3) >> 1;
+
+const div3 = (x) => x / 3; // Math.floor(x / 3);
+
+const multBinDiv2 = (x1, x2) =>  (x1 * x2) >> 1;
+
+const square = (x) => x * x;
+
+const compose = (...fns) => (...args) => {
+  return fns.reduceRight(
+    (comp, f) => f(comp(...args))
+  );
+}
+
+export const sumOfSquares = (x) => compose(div3, multTriDiv2)(x, add1(x), mult2Add1(x));
+/*
+export const sumOfSquares = (x) => {
+  // 1² + 2² + 3² + ... + n² ≡ (n × (n + 1) × (2n + 1)) / 6
+  return div3(
+    multTriDiv2(x, add1(x), mult2Add1(x))
+  );
+}
+*/
+
+export const squareOfSum = (x) => compose(square, multBinDiv2)(x, add1(x));
+/*
+export const squareOfSum = (x) => {
+  // sum(1:n)^2 ≡ (n × (n + 1) / 2)²
+  return square(
+    multBinDiv2(x, add1(x))
+  );
+}
+*/
+
+// finally:
+export const diffOfSquares = (n) => squareOfSum(n) - sumOfSquares(n);
--- difference-of-squares/difference-of-squares.spec.js
+++ difference-of-squares/difference-of-squares.spec.js
@@ -1,9 +1,11 @@
-import { Squares } from './difference-of-squares';
+import { Squares,
+         diffOfSquares, sumOfSquares, squareOfSum } from './difference-of-squares';
 
 describe('difference-of-squares', () => {
   const squares1 = new Squares(1);
   const squares5 = new Squares(5);
   const squares100 = new Squares(100);
+  const squares500 = new Squares(500);
   const squ_1_000 = new Squares(1_000);
   const squ_5_000 = new Squares(5_000);
   const squ_10_000 = new Squares(10_000);
@@ -46,6 +48,7 @@ describe('difference-of-squares', () => {
     // });
   });
 
+
   describe('Sum the squares of the numbers up to the given number', () => {
     test('sum of squares 1', () => {
       expect(squares1.sumOfSquares).toBe(1);
@@ -79,6 +82,7 @@ describe('difference-of-squares', () => {
     // });
   });
 
+
   describe('Subtract sum of squares from square of sums', () => {
     test('difference of squares 1', () => {
       expect(squares1.difference).toBe(0);
@@ -92,8 +96,8 @@ describe('difference-of-squares', () => {
       expect(squares100.difference).toBe(25164150);
     });
 
-    test('difference of squares 100', () => {
-      expect(squares100.difference).toBe(25164150);
+    test('difference of squares 500', () => {
+      expect(squares500.difference).toBe(15645770750);
     });
 
     test('difference of squares 1_000', () => {
@@ -105,4 +109,114 @@ describe('difference-of-squares', () => {
     //   expect(squ_5_000.difference).toBe(156270827082500);
     // });
   });
+
+
+  //
+  // Functional Portion
+  //
+
+  describe('squareOfSum', () => {
+    test('square of sum 1', () => {
+      expect(squareOfSum(1)).toBe(1);
+    });
+
+    test('square of sum 5', () => {
+      expect(squareOfSum(5)).toBe(225);
+    });
+
+    test('square of sum 100', () => {
+      expect(squareOfSum(100)).toBe(25502500);
+    });
+
+    test('square of sum 500', () => {
+      expect(squareOfSum(500)).toBe(15687562500);
+    });
+
+    test('square of sum 1_000', () => {
+      expect(squareOfSum(1_000)).toBe(250500250000);
+    });
+
+    test('square of sum 5_000', () => {
+      expect(squareOfSum(5_000)).toBe(156312506250000);
+    });
+
+    test('square of sum 10_000', () => {
+      expect(squareOfSum(10_000)).toBe(2500500025000000);
+    });
+
+    // overflow: finds 497141619479951600
+    // test('square of sum 100_000', () => {
+    //   expect(squareOfSum(100_000)).toBe(6553755928790448384);
+    // });
+
+    // overflow - finds 199093026440909060
+    // test('square of sum 500_000', () => {
+    //   expect(squareOfSum(500_000).toBe(15625062500062500000000);
+    // });
+  });
+
+
+  describe('sumOFSquares', () => {
+    test('sum of squares 1', () => {
+      expect(sumOfSquares(1)).toBe(1);
+    });
+
+    test('sum of squares 5', () => {
+      expect(sumOfSquares(5)).toBe(55);
+    });
+
+    test('sum of squares 50', () => {
+      expect(sumOfSquares(50)).toBe(42925);
+    });
+
+    test('sum of squares 100', () => {
+      expect(sumOfSquares(100)).toBe(338350);
+    });
+
+    test('sum of squares 500', () => {
+      expect(sumOfSquares(500)).toBe(41791750);
+    });
+
+    test('sum of squares 750', () => {
+      expect(sumOfSquares(750)).toBe(140906375);
+    });
+
+    test('sum of squares 1_000', () => {
+      expect(sumOfSquares(1_000)).toBe(333833500);
+    });
+
+    // overflow:
+    // test('sum of squares 1_500', () => {
+    //   expect(sumOfSquares(1_500)).toBe(333833500);
+    // });
+  });
+
+
+  describe('diffOfSquares', () => {
+    test('difference of squares 1', () => {
+      expect(diffOfSquares(1)).toBe(0);
+    });
+
+    test('difference of squares 5', () => {
+      expect(diffOfSquares(5)).toBe(170);
+    });
+
+    test('difference of squares 100', () => {
+      expect(diffOfSquares(100)).toBe(25164150);
+    });
+
+    test('difference of squares 500', () => {
+      expect(diffOfSquares(500)).toBe(15645770750);
+    });
+
+    test('difference of squares 1_000', () => {
+      expect(diffOfSquares(1_000)).toBe(250166416500);
+    });
+
+    // overflow
+    // test('difference of squares 5_000', () => {
+    //   expect(diffOfSquares(5_000)).toBe(156270827082500);
+    // });
+  });
+
 });
