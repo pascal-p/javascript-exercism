71492cfe502dd5702090ba072ec1619f48ae99ac
Pascal P <lacsap_666@yahoo.fr>
2021-05-29 09:36:59 +1200

    encode: base cases

---
+++ variable-length-quantity/variable-length-quantity.js
@@ -0,0 +1,13 @@
+/*
+  
+ */
+
+export const encode = (vnum) => {
+  // throw new Error('Remove this statement and implement this function');
+
+  if (vnum.length === 1 && vnum[0] <= 0x7f) return vnum
+};
+
+export const decode = () => {
+  throw new Error('Remove this statement and implement this function');
+};
